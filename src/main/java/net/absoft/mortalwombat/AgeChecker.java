08a0d3a75135b4adedac08a4a650f17a6af46aef
Dmytro Chepelev <dmytro.chepelev@gmail.com>
2022-04-06 13:00:04 +0300

    change MIN_AGE at AgeChecker.java to 21

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,7 +1,7 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private final int MIN_AGE = 21;
 
     public boolean canPlayGmae (int age) {
         return age >= MIN_AGE;
